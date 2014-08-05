be5c265e1f3b67466370f67b719ffb834d309233
unknown <Manu@.(none)>
2014-08-05 19:36:22 +0200

    Fin de la nouvelle gesion de la partie modification de l'ordre des couleurs Ajout de l'effet de fondu pour les nouvelles cases.

--- 2048/matrice.js
+++ 2048/matrice.js
@@ -7,13 +7,13 @@ var entier = ( typeof Int32Array != 'undefined');
 
 function Matrix() {
 	// on cree un vecteur avec toutes les valeurs Ã  0
-	var m = Array.prototype.concat.apply([], arguments);
+	var matrice = Array.prototype.concat.apply([], arguments);
 	var score = Array.prototype.concat.apply([], arguments);
 
-	if (!m.length) {
+	if (!matrice.length) {
 
 		for (var i = 0; i < TAILLE_MATRICE * TAILLE_MATRICE; i++)
-			m.push(0);
+			matrice.push(0);
 
 	}
 
@@ -22,54 +22,89 @@ function Matrix() {
 	}
 	//alert(typeof m);
 
-	this.m = entier ? new Int32Array(m) : m;
+	this.matrice = entier ? new Int32Array(matrice) : matrice;
 	this.score = entier ? new Int32Array(score) : score;
 }
 
 Matrix.prototype = {
 
+	// renvoie vrai si il n'a pas de valeur null dans la matrice
+	_MatriceRemplie : function() {
+
+		// determination de la cellule Ã  remplir
+		for (var i = 0; i < this.matrice.length; i++) {
+			//alert("valeur de la matrice " + this.matrice[i] );
+			if (this.matrice[i] == 0)
+				return false;
+		}
+		//alert("Matrice remplie : vrai");
+		return true;
+
+	},
+
+	// renvoie vrai si la matrice membre et la matrice en argument sont égale
+	_MatriceEgale : function(mat) {
+
+		for (var i = 0; i < this.matrice.length; i++) {
+			if (this.matrice[i] != mat[i]) {
+				return false;
+			}
+		}
+		return true;
+
+	},
+
+	// renvoie vrai si la matrice a au moins une fusion possible
+	_FusionPossible : function() {
+		var retour = false;
+
+		// on recherche deux valeurs cote à cote dans les lignes
+		for (var lig = 0; lig < TAILLE_MATRICE - 1; lig++)
+			for (var col = 0; col < TAILLE_MATRICE; col++)
+				if (this.matrice[lig + col * TAILLE_MATRICE] == this.matrice[lig + 1 + col * TAILLE_MATRICE])
+					retour = true;
+
+		// on rrecommance pour les colonnes
+		for (var col = 0; col < TAILLE_MATRICE - 1; col++)
+			for (var lig = 0; lig < TAILLE_MATRICE; lig++)
+				if (this.matrice[lig + col * TAILLE_MATRICE] == this.matrice[lig + (col + 1) * TAILLE_MATRICE])
+					retour = true;
+
+		return retour;
+
+	},
+
 	// fait faire a la matrice 1/4 de tour dans le sens anti-horaire
 	Rotation : function() {
-		result = new Matrix();
-		var v = this.m;
-		var m = result.m;
+		var result = new Matrix();
 
 		for (var lig = 0; lig < TAILLE_MATRICE; ++lig) {
 			for (var col = 0; col < TAILLE_MATRICE; ++col) {
-				m[lig + col * TAILLE_MATRICE] = v[(TAILLE_MATRICE - col - 1) + lig * TAILLE_MATRICE];
+				result.matrice[lig + col * TAILLE_MATRICE] = this.matrice[(TAILLE_MATRICE - col - 1) + lig * TAILLE_MATRICE];
 			}
 		}
 
 		// on applique la matrice
-		this.m = result.m;
+		this.matrice = result.matrice;
 	},
 
-	// permet de recupÃ©rer la valeur correspondant Ã  la cellule
-	Valeur : function(ligne, colonne) {
-		return this.m[ligne * TAILLE_MATRICE + colonne];
-	},
-
-	// permet d'affecter une valeur dans la cellule voulue
-	AffecterValeur : function(ligne, colonne, valeur) {
-		this.m[ligne * TAILLE_MATRICE + colonne] = valeur;
-	},
-
-	// ajoute une nouvelle valeur 2 ou 4 dans une cellule vide
+	// ajoute une nouvelle valeur -2 ou -4 dans une cellule vide
 	// return vrai si OK, faux si il n'y a pas de cellule vide poru ajouter une valeur.
 	// test fin de partie
 	PopCase : function() {
 
-		var celluleVide = [];
-		var m = this.m;
-
-		// determination de la cellule Ã  remplir
-		for (var i = 0; i < m.length; i++) {
-			if (m[i] == 0)
-				celluleVide.push(i);
+		if (this._MatriceRemplie()) {
+			//alert("Matrice remplie");
+			return false;
 		}
 
-		if (celluleVide.length == 0)
-			return false;
+		var celluleVide = [];
+
+		// determination de la cellule Ã  remplir
+		for (var i = 0; i < this.matrice.length; i++) {
+			if (this.matrice[i] == 0)
+				celluleVide.push(i);
+		}
 
 		var indiceCelluleVide = Math.floor(Math.random() * celluleVide.length);
 
@@ -78,54 +113,60 @@ Matrix.prototype = {
 		var valeurAjoute = Math.pow(2, Math.floor(Math.random() * 2) + 1);
 		//alert( Math.floor(Math.random() *2));
 
-		m[celluleVide[indiceCelluleVide]] = valeurAjoute;
+		this.matrice[celluleVide[indiceCelluleVide]] = -valeurAjoute;
 
 		// on test pour voir si il y a un mouvement possible
 
-		var retour = false;
-		celluleVide = [];
+		return true;
 
-		// avant le test de mouvement possible il faut s'assurer que le tableau soit plein....
-		for (var i = 0; i < m.length; i++) {
-			if (m[i] == 0)
-				celluleVide.push(i);
+	},
+
+	Mouvement : function() {
+
+		var copieLocal = [];
+
+		for (var i = 0; i < TAILLE_MATRICE * TAILLE_MATRICE; i++)
+			copieLocal.push(this.matrice[i]);
+
+		if (this.MergeGravite() == true) {
+			alert("vous avez gagné.\n Votre score est de " + this.Score());
 		}
 
-		if (celluleVide.length == 0) {
-			// on recherche deux valeurs cote à cote dans les lignes
-			for (var lig = 0; lig < TAILLE_MATRICE - 1; lig++)
-				for (var col = 0; col < TAILLE_MATRICE; col++)
-					if (m[lig + col * TAILLE_MATRICE] == m[lig + 1 + col * TAILLE_MATRICE])
-						retour = true;
+		// pas de changement de la matrice = > pas de pop
+		if (this._MatriceEgale(copieLocal)) {
+			return true;
+		}
 
-			// on rrecommance pour les colonnes
-			for (var col = 0; col < TAILLE_MATRICE - 1; col++)
-				for (var lig = 0; lig < TAILLE_MATRICE; lig++)
-					if (m[lig + col * TAILLE_MATRICE] == m[lig + 1 + col * TAILLE_MATRICE])
-						retour = true;
+		if (this.PopCase() == false) {
+			return false;
 
-		} else // si il y a des cellule vide on ne fini pas la partie....
-			retour = true;
+		}
+		
+		if (this._FusionPossible() == false & this._MatriceRemplie()) {
+			return false;
 
-		return retour;
+		}
 
 	},
 
 	MergeGravite : function() {
 
+		var partieGagne = false;
+
 		for (var col = 0; col < TAILLE_MATRICE; col++) {
+
 			var ValeurNonVideColonne = new Array();
 
 			// on recupere les cellules non vide par colonne
 			for (var lig = TAILLE_MATRICE - 1; lig >= 0; lig--)
-				if (this.m[lig * TAILLE_MATRICE + col] != 0)
-					ValeurNonVideColonne.push(this.m[lig * TAILLE_MATRICE + col]);
+				if (this.matrice[lig * TAILLE_MATRICE + col] != 0)
+					ValeurNonVideColonne.push(this.matrice[lig * TAILLE_MATRICE + col]);
 
 			//alert("Nombre de valeur non vide de la colonne " + col + " : " +ValeurNonVideColonne.length );
 
 			// on vide toutes les cellules de la colonne
-			for (var lig = TAILLE_MATRICE - 1; lig >= 0; lig--)
-				this.m[lig * TAILLE_MATRICE + col] = 0;
+			for (var lig = 0; lig < TAILLE_MATRICE; lig++)
+				this.matrice[lig * TAILLE_MATRICE + col] = 0;
 
 			//alert("Les valeurs non nulls sont : " +ValeurNonVideColonne);
 
@@ -145,20 +186,21 @@ Matrix.prototype = {
 					// sinon la valeur n'est pas fusionnÃ©e
 					valeurInserer = ValeurNonVideColonne[indiceValeurNonNulle];
 
-				//alert("Valeur " + valeurInserer + " inserer dans la colonne " + col + " a la ligne : " +ligneValeurInser);
+				// on insere la valeur en partant du bas de la matrice
+				this.matrice[(ligneValeurInser) * TAILLE_MATRICE + col] = valeurInserer;
+				ligneValeurInser--;
 
+				// on fini la fusion meme si la partie est gagné
 				if (valeurInserer == 2048) {
-					alert("FÃ©licitations, vous avez gagnÃ©!! \nVotre score est de : " + matrice.Score());
-					NouvelleGrille();
+					partieGagne = true;
 
 				}
-
-				// on insere la valeur en partant du bas de la matrice
-				this.m[(ligneValeurInser) * TAILLE_MATRICE + col] = valeurInserer;
-				ligneValeurInser--;
 			}
 
 		}
+
+		return partieGagne;
+
 	},
 
 	// renvoie la matrice sous forme de texte
@@ -172,60 +214,70 @@ Matrix.prototype = {
 			nom = "case" + cellule;
 			baliseP = document.getElementById(nom);
 			baliseTd = baliseP.parentNode;
-			valeurCellule = this.m[cellule];
-			baliseP.innerHTML = valeurCellule > 0 ? this.m[cellule] : " ";
+			valeurCellule = this.matrice[cellule];
+
+			if (valeurCellule > 0)
+				baliseP.innerHTML = this.matrice[cellule];
+			else if (valeurCellule == 0)
+				baliseP.innerHTML = " ";
+			else
+				baliseP.innerHTML = -this.matrice[cellule];
 
 			switch(valeurCellule) {
+			case -2:
+			case -4:
+				{
+					baliseTd.className = "animationNewCell";
+					baliseP.className = "size01";
+					this.matrice[cellule] = -this.matrice[cellule];
+				}
+				break;
 			case 0:
 				{
 					baliseTd.className = "grisflat";
 					baliseP.className = "size01";
 				}
-break;
+				break;
 			case 2:
 			case 4:
-			case 8:
 				{
 					baliseTd.className = "vert";
 					baliseP.className = "size01";
 				}
 				break;
+			case 8:
 			case 16:
-			case 32:
-			case 64:
-			case 128:
 				{
 					baliseTd.className = "bleu";
 					baliseP.className = "size01";
 				}
 				break;
-			case 256:
-			case 512:
+			case 32:
+			case 64:
 				{
 					baliseTd.className = "orange";
 					baliseP.className = "size01";
 				}
 				break;
-			case 1024:
-			case 2048:
+			case 128:
+			case 256:
+			case 512:
 				{
 					baliseTd.className = "violet";
 					baliseP.className = "size02";
 				}
 				break;
-			case 4096:
-			case 5192:
+			case 1024:
+			case 2048:
 				{
 					baliseTd.className = "rouge";
-					baliseP.className = "size02";
-				}
-				break;
-			case 16384:
-				{
-					baliseTd.className = "gris-bleu";
 					baliseP.className = "size03";
 				}
 				break;
+
+			case 4096:
+			case 5192:
+			case 16384:
 			case 32768:
 				{
 					baliseTd.className = "gris";
@@ -268,13 +320,14 @@ function NouvelleGrille() {
 	document.getElementById("resultat").innerHTML = matrice.Score();
 }
 
-
 function MouvementBas() {
 	//document.getElementById("matrice").innerHTML = matrice.MatriceTexte();
 
-	matrice.MergeGravite();
+	var retour = matrice.Mouvement();
 
-	PopEtAffichage();
+	matrice.Affichage();
+	
+	affichageText(retour);
 }
 
 function MouvementHaut() {
@@ -283,12 +336,14 @@ function MouvementHaut() {
 	matrice.Rotation();
 	matrice.Rotation();
 
-	matrice.MergeGravite();
+	var retour = matrice.Mouvement();
 
 	matrice.Rotation();
 	matrice.Rotation();
 
-	PopEtAffichage();
+	matrice.Affichage();
+	
+	affichageText(retour);
 }
 
 function MouvementGauche() {
@@ -296,13 +351,15 @@ function MouvementGauche() {
 
 	matrice.Rotation();
 
-	matrice.MergeGravite();
+	var retour = matrice.Mouvement();
 
 	matrice.Rotation();
 	matrice.Rotation();
 	matrice.Rotation();
 
-	PopEtAffichage();
+	matrice.Affichage();
+	
+	affichageText(retour);
 }
 
 function MouvementDroite() {
@@ -312,22 +369,21 @@ function MouvementDroite() {
 	matrice.Rotation();
 	matrice.Rotation();
 
-	matrice.MergeGravite();
+	var retour = matrice.Mouvement();
 
 	matrice.Rotation();
 
-	PopEtAffichage();
+	matrice.Affichage();
+	
+	affichageText(retour);
 }
 
-function PopEtAffichage() {
-
-	if (matrice.PopCase() == false) {
-		alert("Matrice pleine. Vous avez perdu :( ... \nVotre score est de : " + matrice.Score());
-		NouvelleGrille();
-	} else {
-		matrice.Affichage();
+function affichageText(retour){
+	
+	if(retour == false)
+		alert("Vous avez perdu... Votre score est de " + matrice.Score());
+	else
 		document.getElementById("resultat").innerHTML = matrice.Score();
-	}
 }
 
 function touchePressee(event) {
